3e4e6fc6587c650d057c20aecb4091dbdad77fbf
AlenOrbanic <aorbanic@student.unipu.hr>
2023-10-27 18:02:50 +0200

    ShowFPS v.2 (renamed to visualia)
    Changed fps color to green, also added dynamic color changes.
    Added removal of low hp pulse effect and uderwather overlay (worked until last patch and fixing it right now....).

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using MelonLoader;
-using FpsStatsMod;
+using VisualiaMod;
 
 // Change the below to reflect your mod information. The second line with game information is optional.
-[assembly: MelonInfo(typeof(ShowFPS), "ShowFPS", "1.0.0", "Orba")]
+[assembly: MelonInfo(typeof(visualia), "VisualiaMod", "2.0.0", "Orba")]
 [assembly: MelonGame("DECA Live Operations GmbH", "RotMGExalt")]
---
+++ visualia.cs
@@ -0,0 +1,246 @@
+﻿using UnityEngine;
+using MelonLoader;
+using static MelonLoader.MelonLogger;
+using Il2CppDecaGames.RotMG.Managers;
+using Il2Cpp;
+using System.Collections;
+
+namespace VisualiaMod
+{
+    public class visualia : MelonMod
+    {
+        public static visualia? instance;
+        private bool isMain = false;
+
+        private GameObject? GameControllerObj;
+        private GameObject? FpsStatsObj;
+        private ApplicationManager? ApplicationManagerObj;
+        private GameObject? fpsDisplayObj;
+        private float enableDelay = 0.1f;
+        private float timeToEnableChild;
+
+        private bool isCheckingTextComponent = false;
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            isMain = sceneName == "Main";
+            if (isMain)
+            {
+                Setup();
+            }
+        }
+
+        private void Setup()
+        {
+            GameControllerObj = FindAndLog("GameController");
+            if (GameControllerObj == null)
+            {
+                Msg("GameController not found.");
+                return;
+            }
+
+            ApplicationManagerObj = GetAndLogComponent<ApplicationManager>(GameControllerObj, "ApplicationManager");
+            if (ApplicationManagerObj == null)
+            {
+                Msg("ApplicationManager not found.");
+                return;
+            }
+
+            FpsStatsObj = FindAndLog("Fps/Stats", GameControllerObj);
+            if (FpsStatsObj == null)
+            {
+                Msg("Fps/Stats not found.");
+                return;
+            }
+
+            SafeActivate(FpsStatsObj, "Fps/Stats");
+
+            fpsDisplayObj = FindAndLog("FPS ---------------------------", FpsStatsObj);
+            if (fpsDisplayObj == null)
+            {
+                Msg("FPS Display not found.");
+                return;
+            }
+
+            timeToEnableChild = Time.time + enableDelay;
+
+            string[] childrenNamesToDestroy = {
+                "BG_Image_TEXT", "avg_fps_text", "avg_fps_value",
+                "min_fps_text", "min_fps_value", "max_fps_text", "max_fps_value", "BG_Image_FULL", "BG_Image_BASIC"
+            };
+
+            foreach (string childName in childrenNamesToDestroy)
+            {
+                GameObject? childObj = FindAndLog(childName, fpsDisplayObj);
+                if (childObj != null)
+                {
+                    UnityEngine.Object.Destroy(childObj);
+                }
+            }
+
+            UpdateTextColorAndPosition();
+        }
+
+        private void UpdateTextColorAndPosition()
+        {
+            if (!isCheckingTextComponent)
+            {
+                MelonCoroutines.Start(CheckTextComponentValue()); // Start the MelonCoroutine
+            }
+        }
+        private IEnumerator CheckTextComponentValue()
+        {
+            isCheckingTextComponent = true;
+            while (true)
+            {
+                string[] childrenNames = { "fps_text", "fps_text_value", "ms_text", "ms_text_value" };
+                Vector3[] positions =
+                {
+                    new Vector3(1892.63f, 1041.095f, 0f),
+                    new Vector3(1784.126f, 1041.095f, 0f),
+                    new Vector3(1884.052f, 1021.87f, 0f),
+                    new Vector3(1776.906f, 1022.269f, 0f)
+                };
+
+                for (int i = 0; i < childrenNames.Length; i++)
+                {
+                    GameObject? childObj = FindAndLog(childrenNames[i], fpsDisplayObj);
+                    if (childObj != null)
+                    {
+                        UnityEngine.UI.Text? textComponent = GetAndLogComponent<UnityEngine.UI.Text>(childObj, "Text");
+                        if (textComponent != null)
+                        {
+                            textComponent.fontSize = 20;
+                            textComponent.color = new Color(0, 1, 0, 0.6027f);
+
+                            if (float.TryParse(textComponent.m_Text, out float floatValue))
+                            {
+                                if (IsWholeNumber(floatValue))
+                                {
+                                    Color textColor = GetColorForWholeNumber(floatValue);
+                                    textComponent.color = textColor;
+                                }
+                                else
+                                {
+                                    Color textColor = GetColorForValueWithDecimal(floatValue);
+                                    textComponent.color = textColor;
+                                }
+                            }
+                        }
+                        childObj.transform.position = positions[i];
+                    }
+                }
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
+
+
+        private bool IsWholeNumber(float value)
+        {
+            return Mathf.Approximately(value, Mathf.Round(value));
+        }
+
+        private Color GetColorForWholeNumber(float value)
+        {
+            if (value >= 1 && value <= 20)
+            {
+                return new Color(1, 0, 0, 0.6027f);
+            }
+            else if (value > 20 && value <= 30)
+            {
+                return new Color(1, 0.6004f, 0, 0.6027f);
+            }
+            else if (value > 30 && value <= 50)
+            {
+                return new Color(1, 1, 0.1679f, 0.7238f);
+            }
+            else if (value > 50 && value <= 60)
+            {
+                return new Color(0, 1, 0, 0.6027f);
+            }
+            else
+            {
+                return new Color(1, 0, 0, 0.6027f);
+            }
+        }
+
+        private Color GetColorForValueWithDecimal(float value)
+        {
+            if (value >= 1.0 && value <= 20.0)
+            {
+                return new Color(0, 1, 0, 0.6027f);
+            }
+            else if (value > 50.0 && value <= 100.0)
+            {
+                return new Color(1, 0.6004f, 0, 0.6027f);
+            }
+            else if (value > 20.0 && value <= 50.0)
+            {
+                return new Color(1, 1, 0.1679f, 0.7238f);
+            }
+            else if (value > 100.0)
+            {
+                return new Color(1, 0, 0, 0.6027f);
+            }
+            else
+            {
+                return new Color(1, 0, 0, 0.6027f);
+            }
+        }
+
+
+
+        private GameObject? FindAndLog(string objectName, GameObject? parent = null)
+        {
+            GameObject? foundObj = parent == null ? GameObject.Find(objectName) : FindObject(parent, objectName);
+            return foundObj;
+        }
+
+        private T? GetAndLogComponent<T>(GameObject parent, string componentName) where T : Component
+        {
+            T? component = parent.GetComponent<T>();
+            return component;
+        }
+
+        private void SafeActivate(GameObject obj, string objName)
+        {
+            try
+            {
+                obj.SetActive(true);
+            }
+            catch (Exception e)
+            {
+                Msg($"ERROR: Exception while trying to activate {objName} object: {e}");
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (fpsDisplayObj != null)
+            {
+                if (!fpsDisplayObj.activeInHierarchy)
+                {
+                    fpsDisplayObj.SetActive(true);
+                }
+            }
+            else if (isMain && Time.time >= timeToEnableChild)
+            {
+                fpsDisplayObj = FindAndLog("FPS ---------------------------", FpsStatsObj);
+                if (fpsDisplayObj != null)
+                {
+                    fpsDisplayObj.SetActive(true);
+                }
+            }
+        }
+
+        public static GameObject? FindObject(GameObject parent, string name)
+        {
+            Transform[] trs = parent.GetComponentsInChildren<Transform>(true);
+            foreach (Transform t in trs)
+            {
+                if (t.name == name) { return t.gameObject; }
+            }
+            return null;
+        }
+    }
+}
